2527241cd09d4fb1a81e00cc80a14774c5ff1449
aadya <110504381+adex1a@users.noreply.github.com>
2026-01-07 21:31:07 -0800

    added basic nav bar

--- personal-website/src/components/Header.jsx
+++ personal-website/src/components/Header.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import "./Header.css";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <header>
       <h1 className="Header-title">Aadya Sharma</h1>
+      <nav className="Header-navbarContainer">
+        <Link to="/" className="Header-navbarLink">
+          Home
+        </Link>
+      </nav>
     </header>
   );
 };
