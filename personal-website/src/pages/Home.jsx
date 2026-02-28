8e0826a9535c4402391e1fd27a851953ffcc6053
aadya <110504381+adex1a@users.noreply.github.com>
2026-02-27 23:28:03 -0500

    got inline links on homepage working

--- personal-website/src/pages/Home.jsx
+++ personal-website/src/pages/Home.jsx
@@ -1,29 +1,48 @@
 import React from "react";
 import "./Home.css";
+import { NavLink } from "react-router-dom";
 
 const Home = () => {
   return (
-    <div className="bg">
-      <h1 className="Home-text">Hello, world!</h1>
-      <div className="Home-text Home-smallText">
-        I'm a writer, game developer, musician, and student at Massachusetts
-        Institute of Technology.
+    <>
+      <div className="bg">
+        <h1 className="Home-text">Hello, world!</h1>
+        <div className="Home-text Home-smallText">
+          I'm a <NavLink to="/writing">writer</NavLink>,{" "}
+          <NavLink to="/games">game developer</NavLink>,{" "}
+          <NavLink to="/music">musician</NavLink>, and{" "}
+          <NavLink to="/about">student</NavLink> at Massachusetts Institute of
+          Technology.
+        </div>
+        <h2 className="Home-text" style={{ textAlign: "center" }}>
+          Talk to me!
+        </h2>
+        <ul className="Home-contactList">
+          -{" "}
+          <a
+            href="https://www.linkedin.com/in/aadya-sharma-/"
+            target="_blank"
+            rel="noreferrer noopener"
+            style={{ zIndex: 1 }}
+            className="Home-text Home-smallText"
+          >
+            LinkedIn
+          </a>
+          {/* <a
+            href="mailto:aadya314@mit.edu"
+            target="_blank"
+            rel="noreferrer noopener"
+            style={{ zIndex: 1 }}
+            className="Home-text Home-smallText"
+          >
+            Email
+          </a> */}
+          <div className="Home-text Home-smallText">
+            - Email: aadya314 [at] mit [dot] edu
+          </div>
+        </ul>
       </div>
-      <h2 className="Home-text" style={{ textAlign: "center" }}>
-        Contact:
-      </h2>
-      {/* <a
-        href="https://www.linkedin.com/in/aadya-sharma-/"
-        target="_blank"
-        rel="noreferrer noopener"
-        style={{ zIndex: -100 }}
-      >
-        Linkedin
-      </a> */}
-      <div className="Home-text Home-smallText">
-        - Email: aadya314 [at] mit [dot] edu
-      </div>
-    </div>
+    </>
   );
 };
 export default Home;
